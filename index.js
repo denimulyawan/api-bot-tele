42a258582b3b703d8ac6529e9742fe28c6ff573a
farhan ramadhan <nak.rokesselalu@gmail.com>
2024-03-18 11:25:34 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/message", async (req, res) => {
+  const {name, email, message} = req.body
+
+  const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: false });
+
+  const teleMessage = `Hi Deni,\nyou have a new message from\nName: ${name}\nemail: ${email}\n\nThe message is: \n${message}`;
+
+  try {
+    await bot.sendMessage(TELEGRAM_CHAT_ID, teleMessage);
+    return res.send({ message: "Message sent to telegram"});
+  } catch (error) {
+    console.log(error);
+    return res.send({ message: "Error sending message to telegram"}).status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
